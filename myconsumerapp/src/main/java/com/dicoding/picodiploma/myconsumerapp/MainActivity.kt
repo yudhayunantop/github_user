c92bd63387e4241db28e401273ab045bdc59c135
Yudha Yunanto Putra <61044350+yudhayunantop@users.noreply.github.com>
2020-11-24 17:27:24 +0700

    commit

--- myconsumerapp/src/main/java/com/dicoding/picodiploma/myconsumerapp/MainActivity.kt
+++ myconsumerapp/src/main/java/com/dicoding/picodiploma/myconsumerapp/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.dicoding.picodiploma.myconsumerapp
 
 import android.database.ContentObserver
-import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.picodiploma.myconsumerapp.adapter.ListUserAdapter
-import com.dicoding.picodiploma.myconsumerapp.db.UserContract
 import com.dicoding.picodiploma.myconsumerapp.db.UserContract.FavoriteColumns.Companion.CONTENT_URI
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 private lateinit var adapter: ListUserAdapter
 
@@ -21,37 +23,40 @@ class MainActivity : AppCompatActivity() {
         rv_fav.setHasFixedSize(true)
 
         supportActionBar?.title = "Favorite"
-
         adapter = ListUserAdapter()
         adapter.notifyDataSetChanged()
 
         rv_fav.layoutManager = LinearLayoutManager(this)
         rv_fav.adapter = adapter
 
-        supportActionBar?.title = "Consumer Favorite"
+        supportActionBar?.title = "Favorite"
 
         val handlerThread = HandlerThread("DataObserver")
         handlerThread.start()
         val handler = Handler(handlerThread.looper)
         val myObserver = object : ContentObserver(handler){
-
+            override fun onChange(self: Boolean) {
+                loadData()
+            }
         }
         contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
+        loadData()
 
-        val cursor = contentResolver?.query(CONTENT_URI, null, null, null, null)
-        var data = cursor?.let { mapCursortoArrayList(it) }
-        adapter.setData(data!!)
 
     }
 
-    fun mapCursortoArrayList(favoriteUser: Cursor): ArrayList<User>{
-        val userList = ArrayList<User>()
-        while (favoriteUser.moveToNext()){
-            val avatar = favoriteUser.getString(favoriteUser.getColumnIndexOrThrow((UserContract.FavoriteColumns.AVATAR)))
-            val username = favoriteUser.getString(favoriteUser.getColumnIndexOrThrow((UserContract.FavoriteColumns.USERNAME)))
-            userList.add(User(avatar, username))
+    fun loadData() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val deferredUsers = async(Dispatchers.IO) {
+                val cursor = contentResolver?.query(CONTENT_URI, null, null, null, null)
+                ConvertHelper.mapCursortoArrayList(cursor!!)
+
+            }
+            val pengguna = deferredUsers.await()
+            adapter.setData(pengguna)
+
         }
-        return userList
     }
+
 
 }
